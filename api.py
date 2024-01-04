c2132b89f13f47269c6cc783d99b1114892a969d
root <root@ai.smartlytechs.com>
2024-01-04 13:04:24 +0100

    working server

--- api.py
+++ api.py
@@ -60,30 +60,50 @@ def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+import requests
+
+
+def send_to_diffusion2(image_path, txt):
+    # URL of the GPU server where you'll upload the image
+    gpu_server_url = 'http://62.67.51.161:5000/upload'  # Replace with your GPU server's URL
+    file_name = image_path.split("/")[-1]
+
+    try:
+        with open(image_path, 'rb') as image_file:
+            files = {
+                'image': image_file,
+            }
+            data = {
+                'type': txt,
+                'name': file_name
+            }
+
+            response = requests.post(gpu_server_url, files=files, data=data)
+
+            if response.status_code == 200:
+                print("Files successfully uploaded to GPU server")
+                return response.text  # Return the response if required
+            else:
+                print("Failed to upload files to GPU server")
+                return None
+    except FileNotFoundError as e:
+        print(f"File not found: {e.filename}")
+        return None
+
+@app.route('/server_2_call', methods=['POST'])
+def upload_image():
+    uploaded_image = request.files['image']
+    uploaded_file_name = request.form['path']
+    
+    uploaded_image.save(uploaded_file_name)  # Save the image to a file
+
+    # Return a response if needed (optional)
+    return 'Image received and processed on GPU server'
+
 # Define a route for the home page
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-@app.route('/not_revised', methods=['POST'])
-def upload_image():
-	if 'file' not in request.files:
-		flash('No file part')
-		return redirect(request.url)
-	file = request.files['file']
-	if file.filename == '':
-		flash('No image selected for uploading')
-		return redirect(request.url)
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		#print('upload_image filename: ' + filename)
-		flash('Image successfully uploaded and displayed below')
-		return render_template('upload.html', filename=filename)
-	else:
-		flash('Allowed image types are -> png, jpg, jpeg, gif')
-		return redirect(request.url)
 
 
 @app.route('/route_for_button_1')
@@ -91,6 +111,11 @@ def route_for_button_1():
     # Fetch data for button 1 (replace this with your logic)
     start = time.time()
     img_path = infere_parser("datalake/image","datalake/image-parse-v3")
+    print(img_path)
+    np = img_path.split(".")
+    path_np = np[0]+ "2.npy"
+    #send_to_diffusion2(img_path,"parse_orange")
+    #send_to_diffusion2(path_np,"parse_orange")
     end = time.time()
     parse_location = f"static/images/hmp_{img_path.split('/')[-1]}"
     shutil.copy(img_path, parse_location)
@@ -119,10 +144,10 @@ def route_for_button_2():
     agnostic,agnostic_mask = get_im_parse_agnostic_original(im_parse, im_parse_np, pose_data)
     out_path = join("datalake","image-parse-agnostic-v3.2", parse_name)
     agnostic.save(out_path)
-
+    #send_to_diffusion2(out_path, "agnostic_black")
     with open(join("datalake","image-parse-agnostic-v3.2", parse_name_npy), 'wb') as f:
         np.save(f, agnostic_mask)
-        
+    #send_to_diffusion2(join("datalake","image-parse-agnostic-v3.2", parse_name_npy), "agnostic_black")    
     end = time.time()
     parse_location = f"static/images/agp_{im_name}"
     shutil.copy(out_path, parse_location)
@@ -150,6 +175,7 @@ def route_for_button_3():
     agnostic = get_img_agnostic_human(rgb_model, im_parse_np, pose_data)
     out_path = join("datalake","agnostic-v3.2", im_name)
     agnostic.save(out_path)
+    #send_to_diffusion2(out_path, "agnostic_gray")
     end = time.time()
     parse_location = f"static/images/agh_{im_name}"
     shutil.copy(out_path, parse_location)
@@ -168,7 +194,7 @@ def route_for_button_4():
     exe_bin_file = "./build/examples/openpose/openpose.bin"
     start = time.time()
     os.chdir("/root/diffusion_root/CIHP_PGN/openpose")
-    cmd_ = f"{exe_bin_file} --image_dir {in_dir} --disable_blending --write_images {out_dir} --hand --write_json {json_dir} --display 0"
+    cmd_ = f"{exe_bin_file} --image_dir {in_dir} --disable_blending --write_images {out_dir} --hand --write_json {json_dir} --display 0 --net_resolution '64x64'"
     success = os.system(cmd_)
     data = {
 		"text": "Faild",
@@ -181,8 +207,11 @@ def route_for_button_4():
         im_name = f.read()
         ext="."+im_name.split(".")[-1]
         im_name_datalake = im_name.replace(ext,"_rendered.png")
+        im_name_datalake_json = im_name.replace(ext,"_keypoints.json")
         parse_location = f"static/images/pose_{im_name}"
         shutil.copy(join(out_dir,im_name_datalake), parse_location)
+        #send_to_diffusion2(join(out_dir,im_name_datalake),"openpose_render")
+        #send_to_diffusion2(join(json_dir,im_name_datalake_json),"openpose_json")
         cv2.imwrite(parse_location, image_resize(cv2.imread(parse_location), height=256))
         data = {
 			"text": f"processed in {round((end-start),2)} seconds",
@@ -213,6 +242,7 @@ def route_for_button_6():
     # Fetch data for button 1 (replace this with your logic)
     start = time.time()
     img_path = infere_cloth_mask("/root/diffusion_root/CIHP_PGN/datalake/cloth","/root/diffusion_root/CIHP_PGN/datalake/cloth-mask")
+    #send_to_diffusion2(img_path, "cloth_mask")
     end = time.time()
     parse_location = f"static/images/cm_{img_path.split('/')[-1]}"
     shutil.copy(img_path, parse_location)
@@ -247,18 +277,50 @@ def route_for_button_7():
 def route_for_button_8():
     # Fetch data for button 1 (replace this with your logic)
     start = time.time()
-    route_for_button_1()
-    route_for_button_4()
-    route_for_button_2()
-    route_for_button_3()
-    route_for_button_5()
-    route_for_button_6()
-    route_for_button_7()
+    infere_parser("datalake/image","datalake/image-parse-v3")
+    ##############################################
+    in_dir = "/root/diffusion_root/CIHP_PGN/datalake/image"
+    out_dir = "/root/diffusion_root/CIHP_PGN/datalake/openpose_img"
+    json_dir = "/root/diffusion_root/CIHP_PGN/datalake/openpose_json"
+    exe_bin_file = "./build/examples/openpose/openpose.bin"
+    os.chdir("/root/diffusion_root/CIHP_PGN/openpose")
+    cmd_ = f"{exe_bin_file} --image_dir {in_dir} --disable_blending --write_images {out_dir} --hand --write_json {json_dir} --display 0 --net_resolution '64x64'"
+    success = os.system(cmd_)
+    if success == 1:
+        print("true succss")
+    #########################################
+    os.chdir("/root/diffusion_root/CIHP_PGN")
+    f = open("name.txt", "r")
+    im_name = f.read()
+    print(im_name)
+    im_parse, im_parse_np, pose_data, parse_name, parse_name_npy = read_pose_parse("datalake",im_name)
+    agnostic,agnostic_mask = get_im_parse_agnostic_original(im_parse, im_parse_np, pose_data)
+    out_path = join("datalake","image-parse-agnostic-v3.2", parse_name)
+    agnostic.save(out_path)
+
+    with open(join("datalake","image-parse-agnostic-v3.2", parse_name_npy), 'wb') as f:
+        np.save(f, agnostic_mask)
+    ########################################
+    rgb_model = Image.open(join("datalake","image", im_name))
+    agnostic = get_img_agnostic_human(rgb_model, im_parse_np, pose_data)
+    out_path = join("datalake","agnostic-v3.2", im_name)
+    agnostic.save(out_path)
+    ############################################
+    infer_densepose("datalake/image","datalake/image-densepose")
+    ############################################
+    img_path = infere_cloth_mask("/root/diffusion_root/CIHP_PGN/datalake/cloth","/root/diffusion_root/CIHP_PGN/datalake/cloth-mask")
+    ############################################
+    out_path = "datasets/HR_VITON_outs"
+    infer_hr_viton("datalake",out_path,"pairs.txt")
+    paired = os.listdir(out_path)
+    im_name = paired[0] if "grid" in paired[1] else paired[1]
+    parse_location = f"static/images/paired_{im_name}"
+    shutil.copy(join(out_path, im_name), parse_location)
+    cv2.imwrite(parse_location, image_resize(cv2.imread(parse_location), height=256))
     end = time.time()
-
     data = {
-        "text": f"processed in {round((end-start),2)} seconds"
-
+        "text": f"processed in {round((end-start),2)} seconds",
+        "image": parse_location
     }
     print(data)
     return jsonify(data)
@@ -277,9 +339,11 @@ def upload_person():
     uploaded_file = request.files['file_p']
     img_name = uploaded_file.filename
     uploaded_file.save(join("datalake", "image", img_name))
+
     img = Image.open(join("datalake", "image", img_name))
     img_resize = img.resize((w, h))
     img_resize.save(join("datalake", "image", img_name))
+    #send_to_diffusion2(join("datalake", "image", img_name),"person")
     end = time.time()
     parse_location = f"static/images/paired_{img_name}"
     shutil.copy(join("datalake", "image", img_name), parse_location)
@@ -293,6 +357,7 @@ def upload_person():
     f = open("name.txt", "a")
     f.write(img_name)
     f.close()
+    #send_to_diffusion2("name.txt","names")
     # Return a response to the front end
     return jsonify(data)
         
@@ -320,6 +385,7 @@ def upload_cloth():
     img = Image.open(join("datalake", "cloth", img_name))
     img_resize = img.resize((w, h))
     img_resize.save(join("datalake", "cloth", img_name))
+    #send_to_diffusion2(join("datalake", "cloth", img_name),"cloth")
     end = time.time()
     parse_location = f"static/images/unpaired_{img_name}"
     shutil.copy(join("datalake", "cloth", img_name), parse_location)
@@ -332,4 +398,4 @@ def upload_cloth():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=5000)
--- tf_gpu.py
+++ tf_gpu.py
@@ -1,81 +1,65 @@
 from __future__ import print_function
 import argparse
-from datetime import datetime
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from os.path import join
-import sys
 import time
-import scipy.misc
-import scipy.io as sio
-import cv2
-import argparse
-from glob import glob
-import time
-# os.environ["CUDA_VISIBLE_DEVICES"]="0"
-
 import tensorflow as tf
-tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
-import logging
-tf.get_logger().setLevel(logging.ERROR)
-
 import numpy as np
 from PIL import Image
 from utils.image_reade_inf import *
-from utils.ops import  *
+from utils.ops import *
 from utils.utils import *
 from utils.model_pgn import *
-
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
+import logging
+tf.get_logger().setLevel(logging.ERROR)
+#export LD_LIBRARY_PATH=/media/HDD2/VITON/anaconda/pkgs/cudatoolkit-10.0.130-0/lib
 argp = argparse.ArgumentParser(description="Inference pipeline")
 argp.add_argument('-i',
                   '--directory',
-                  type=str, help='Path of the input dir',
+                  type=str,
+                  help='Path of the input dir',
                   default='./datasets/images')
 argp.add_argument('-o',
                   '--output',
-                  type=str, help='Path of the input dir',
+                  type=str,
+                  help='Path of the input dir',
                   default='./datasets/output')
 
 args = argp.parse_args()
 
+def load_and_preprocess_image(image_path):
+    
+    print(image_path, type(str(image_path)))
+    img = tf.keras.preprocessing.image.load_img(image_path, grayscale=False, color_mode='rgb', target_size=None, interpolation='nearest')
+    img_rev = tf.reverse(img, tf.stack([1]))  # Reverse image
+    return img, img_rev
+
 def infere_parser(in_path, out_):
+   
     start = time.time()
     base = in_path
     image_list_inp = []
     for img in os.listdir(base):
         image_list_inp.append(join(base, img))
     print(image_list_inp)
-   
 
+    image_batch,image_rev_batch = load_and_preprocess_image(image_list_inp[0])
+    
     N_CLASSES = 20
     NUM_STEPS = len(image_list_inp)
 
-    """Create the model and start the evaluation process."""
-    # Create queue coordinator.
-    coord = tf.train.Coordinator()
-    
-    # Load reader.
-    with tf.name_scope("create_inputs"):
-        reader = ImageReader(image_list_inp, None, False,
-                             False, False, coord)
-        image = reader.image
-        image_rev = tf.reverse(image, tf.stack([1]))
-        image_list = reader.image_list
-
     out_dir = out_ #"/root/diffusion_root/TestSet_LC/image-parse-v3"
 
-    image_batch = tf.stack([image, image_rev])
+    image_batch = tf.stack([image_batch, image_rev_batch])
     h_orig, w_orig = tf.to_float(tf.shape(image_batch)[1]), tf.to_float(tf.shape(image_batch)[2])
     scale = 0.5
     image_batch050 = tf.image.resize_images(image_batch, tf.stack([tf.to_int32(tf.multiply(h_orig, scale)), tf.to_int32(tf.multiply(w_orig, scale))]))
-         
+        
     # Create network.
-    with tf.variable_scope('', reuse=False):
-        net_100 = PGNModel({'data': image_batch050}, is_training=False, n_classes=N_CLASSES)
-    end = time.time()
-    print(f"[Not Solved]first bottle neck {round((end-start)/ 60,2)} minutes")
-    
-    # parsing net
+    net_100 = PGNModel({'data': image_batch050}, is_training=False, n_classes=N_CLASSES)
+        # parsing net
     parsing_out1_100 = net_100.layers['parsing_fc']
     
     # combine resize
@@ -98,38 +82,29 @@ def infere_parser(in_path, out_):
     pred_scores = tf.reduce_max(raw_output_all, axis=3)
     raw_output_all = tf.argmax(raw_output_all, axis=3)
     pred_all = tf.expand_dims(raw_output_all, dim=3) # Create 4-d tensor.
-
-    start2 = time.time()
+    print("what about here", time.time() - start)
     # Which variables to load.
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    sess = tf.Session(config=config)
-    init = tf.global_variables_initializer()
-    sess.run(init)
-    sess.run(tf.local_variables_initializer())
+    st = time.time()
+    sess = tf.Session()
     restore_var = tf.global_variables()
-    # Load weights.
+    print("fff", time.time() - st)
+
     RESTORE_FROM = './checkpoint/CIHP_pgn'
     loader = tf.compat.v1.train.Saver(var_list=restore_var)
-    if RESTORE_FROM is not None:
-        if load(loader, sess, RESTORE_FROM):
-            print(" [*] Load SUCCESS")
-        else:
-            print(" [!] Load failed...")
-    # Start queue threads.
-            
-    threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+    load(loader, sess, RESTORE_FROM)
     end2 = time.time()
-    print(f"[Not solved]second bottle neck {round((end2-start2)/ 60,2)} minutes")
+    print(f"[Not solved]second bottle neck {round((end2-start),2)} seconds")
     img_id = ""
     print("before start", NUM_STEPS)
+    # device = '/CPU:0'#'/device:GPU:0'
+    # with tf.device(device):
     for step in range(NUM_STEPS):
         print("Started",step)
         start = time.time()
         parsing_, scores = sess.run([pred_all, pred_scores])
         end = time.time()
-        print(f"[Solved]third bottle neck {round((end-start)/ 60,2)} minutes")
-        img_split = image_list[step].split('/')
+        print(f"[Solved]third bottle neck {round((end-start),2)} minutes")
+        img_split = image_list_inp[step].split('/')
         img_id = img_split[-1][:-4]
 
         t =parsing_[0, :, :, 0]
@@ -142,20 +117,21 @@ def infere_parser(in_path, out_):
         parsing_im = Image.fromarray(msk[0])
         parsing_im = parsing_im.convert('P')
         parsing_im.save('{}/{}_vis.png'.format(out_dir, img_id))
-  
+
     
-    coord.request_stop()
-    coord.join(threads)
+    # coord.request_stop()
+    # coord.join(threads)
 
     return '{}/{}_vis.png'.format("./datalake/image-parse-v3", img_id)
     
+    
 
 
-# if __name__ == '__main__':
-#     start = time.time()
-#     in_path, out_ = "/root/diffusion_root/CIHP_PGN/datalake/image", "/root/diffusion_root/CIHP_PGN/datalake/image-parse-v3"
-#     infere_parser(in_path, out_)
-#     end = time.time()
-#     print(f"time taken {round((end-start)/ 60,2)} minutes")
+if __name__ == '__main__':
+    start = time.time()
+    in_path, out_ = "/root/diffusion_root/CIHP_PGN/datalake/image", "/root/diffusion_root/CIHP_PGN/datalake/image-parse-v3"
+    infere_parser(in_path, out_)
+    end = time.time()
+    print(f"time taken {round((end-start),2)} seconds")
 
-#################################################################
+################################################################
